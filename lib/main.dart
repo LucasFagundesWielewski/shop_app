58a550d007c6f859d174c9b5708d9f48957c5288
Lucas <lucas.fwielewski@gmail.com>
2024-09-20 08:53:16 -0300

    feat: create counter provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/views/products_overview_page.dart';
 import 'package:shop_app/views/products_detail_page.dart';
-import 'package:shop_app/views/counter_page.dart';
 import 'package:shop_app/utils/app_routes.dart';
 
 void main() {
@@ -11,7 +10,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,7 +23,7 @@ class MyApp extends StatelessWidget {
       ),
       home: ProductsOverviewPage(),
       routes: {
-        AppRoutes.PRODUCT_DETAIL: (context) => const CounterPage(),
+        AppRoutes.PRODUCT_DETAIL: (context) => const ProductDetailPage(),
       },
       debugShowCheckedModeBanner: false,
     );
--- lib/views/counter_page.dart
+++ lib/views/counter_page.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
+import '../providers/counter.dart';
 
-class CounterPage extends StatelessWidget {
+class CounterPage extends StatefulWidget {
   const CounterPage({Key? key}) : super(key: key);
 
   @override
+  _CounterPageState createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  @override
   Widget build(BuildContext context) {
+    final provider = CounterProvider.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text("Exemplo contador"),
+      ),
+      body: Column(
+        children: [
+          Text(provider?.state.value.toString() ?? ""),
+          IconButton(
+            onPressed: () {
+              setState(() {
+                provider?.state.inc();
+              });
+            },
+            icon: Icon(Icons.add),
+          ),
+          IconButton(
+            onPressed: () {
+              setState(() {
+                provider?.state.dec();
+              });
+            },
+            icon: Icon(Icons.remove),
+          ),
+        ],
       ),
     );
   }
