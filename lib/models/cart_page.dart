5774625d38d9efc723ee5b8b1872173dc08f48c8
Lucas <lucas.fwielewski@gmail.com>
2024-10-01 23:25:15 -0300

    feat: added total in cart screen

--- lib/models/cart_page.dart
+++ lib/models/cart_page.dart
@@ -6,9 +6,32 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Carrinho'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Carrinho'),
+        ),
+        body: Column(
+          children: [
+            Card(
+              child: Row(
+                children: [
+                  Text(
+                    'Total',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  Chip(
+                      backgroundColor: Theme.of(context).primaryColor,
+                      label: Text('R\$ 100.00',
+                          style: TextStyle(
+                              color: Theme.of(context)
+                                  .primaryTextTheme
+                                  .titleLarge
+                                  ?.color))),
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
