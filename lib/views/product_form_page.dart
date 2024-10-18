2efdcd850178f9cabf5482298cbf67a28fc7efae
Lucas <lucas.fwielewski@gmail.com>
2024-10-17 22:51:47 -0300

    feat: added form validator

--- lib/views/product_form_page.dart
+++ lib/views/product_form_page.dart
@@ -37,7 +37,21 @@ class _ProductFormPageState extends State<ProductFormPage> {
     }
   }
 
+  bool isValideImageUrl(String url) {
+    final isValidProtocol = url.toLowerCase().startsWith('http://') ||
+        url.toLowerCase().startsWith('https://');
+    final isValidExtension = url.toLowerCase().endsWith('.png') ||
+        url.toLowerCase().endsWith('.jpg') ||
+        url.toLowerCase().endsWith('.jpeg');
+    return isValidProtocol && isValidExtension;
+  }
+
   void _submitForm() {
+    final isValid = _formKey.currentState?.validate();
+
+    if (!isValid!) {
+      return;
+    }
     _formKey.currentState?.save();
     final newProduct = Product(
       id: DateTime.now().toString(),
@@ -73,46 +87,66 @@ class _ProductFormPageState extends State<ProductFormPage> {
                   FocusScope.of(context).requestFocus(_priceFocus);
                 },
                 onSaved: (name) => _formData['name'] = name ?? '',
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Preço'),
-                textInputAction: TextInputAction.next,
-                focusNode: _priceFocus,
-                keyboardType: const TextInputType.numberWithOptions(
-                  decimal: true,
-                ),
-                onFieldSubmitted: (_) {
-                  FocusScope.of(context).requestFocus(_descriptionFocus);
+                validator: (name) {
+                  return name?.trim().isEmpty == true
+                      ? 'Informe um nome válido'
+                      : null;
                 },
-                onSaved: (price) =>
-                    _formData['price'] = double.parse(price ?? '0.0'),
               ),
               TextFormField(
-                decoration: const InputDecoration(labelText: 'Descrição'),
-                textInputAction: TextInputAction.next,
-                focusNode: _descriptionFocus,
-                keyboardType: TextInputType.multiline,
-                maxLines: 3,
-                onSaved: (description) =>
-                    _formData['description'] = description ?? '',
-              ),
+                  decoration: const InputDecoration(labelText: 'Preço'),
+                  textInputAction: TextInputAction.next,
+                  focusNode: _priceFocus,
+                  keyboardType: const TextInputType.numberWithOptions(
+                    decimal: true,
+                  ),
+                  onFieldSubmitted: (_) {
+                    FocusScope.of(context).requestFocus(_descriptionFocus);
+                  },
+                  onSaved: (price) =>
+                      _formData['price'] = double.parse(price ?? '0.0'),
+                  validator: (price) {
+                    final parsedPrice = double.tryParse(price ?? '');
+                    return parsedPrice == null || parsedPrice <= 0
+                        ? 'Informe um preço válido'
+                        : null;
+                  }),
+              TextFormField(
+                  decoration: const InputDecoration(labelText: 'Descrição'),
+                  textInputAction: TextInputAction.next,
+                  focusNode: _descriptionFocus,
+                  keyboardType: TextInputType.multiline,
+                  maxLines: 3,
+                  onSaved: (description) =>
+                      _formData['description'] = description ?? '',
+                  validator: (description) {
+                    return description?.trim().isEmpty == true
+                        ? 'Informe uma descrição válida'
+                        : null;
+                  }),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
                   Expanded(
                     child: TextFormField(
-                      decoration:
-                          const InputDecoration(labelText: 'URL da Imagem'),
-                      focusNode: _imageUrlFocus,
-                      keyboardType: TextInputType.url,
-                      textInputAction: TextInputAction.done,
-                      controller: _imageUrlController,
-                      onFieldSubmitted: (_) {
-                        _submitForm();
-                      },
-                      onSaved: (imageUrl) =>
-                          _formData['imageUrl'] = imageUrl ?? '',
-                    ),
+                        decoration:
+                            const InputDecoration(labelText: 'URL da Imagem'),
+                        focusNode: _imageUrlFocus,
+                        keyboardType: TextInputType.url,
+                        textInputAction: TextInputAction.done,
+                        controller: _imageUrlController,
+                        onFieldSubmitted: (_) {
+                          _submitForm();
+                        },
+                        onSaved: (imageUrl) =>
+                            _formData['imageUrl'] = imageUrl ?? '',
+                        validator: (imageUrl) {
+                          return imageUrl?.trim().isEmpty == true
+                              ? 'Informe uma URL válida'
+                              : !isValideImageUrl(imageUrl!)
+                                  ? 'Informe uma URL válida'
+                                  : null;
+                        }),
                   ),
                   Container(
                     height: 100,
