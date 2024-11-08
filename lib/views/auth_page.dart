56fe5a525e030b04d0e17fa65d79a8f13838b6a5
Lucas <lucas.fwielewski@gmail.com>
2024-11-07 22:08:07 -0300

    feat: added text to login screen

--- lib/views/auth_page.dart
+++ lib/views/auth_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AuthPage extends StatelessWidget {
-  const AuthPage({Key? key}) : super(key: key);
+  const AuthPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,13 +15,46 @@ class AuthPage extends StatelessWidget {
                   Color.fromRGBO(215, 117, 255, 0.5),
                   Color.fromRGBO(255, 188, 117, 0.9),
                 ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
               ),
             ),
           ),
-          Container(
+          SizedBox(
             width: double.infinity,
             child: Column(
-              children: [],
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 70,
+                  ),
+                  transform: Matrix4.rotationZ(-8 * 3.1415926535 / 180)
+                    ..translate(-10.0),
+                  decoration: BoxDecoration(
+                    color: Colors.deepOrange.shade900,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 8,
+                        spreadRadius: 2,
+                        offset: Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  child: const Text(
+                    'Minha Loja',
+                    style: TextStyle(
+                      fontSize: 45,
+                      fontFamily: 'Anton',
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
             ),
           )
         ],
