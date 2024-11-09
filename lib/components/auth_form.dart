6c935909e04fbb67ef4bb767612e353de73c4386
Lucas <lucas.fwielewski@gmail.com>
2024-11-09 15:02:04 -0300

    feat: added all fields

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+enum AuthMode { Signup, Login }
+
 class AuthForm extends StatefulWidget {
   const AuthForm({super.key});
 
@@ -8,8 +10,100 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _passwordController = TextEditingController();
+  final AuthMode _authMode = AuthMode.Login;
+  final Map<String, String> _authData = {
+    'email': '',
+    'password': '',
+  };
+
+  void _submit() {
+    if (_authMode == AuthMode.Login) {
+      // Login
+    } else {
+      // Signup
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Text('Auth Form');
+    final deviceSize = MediaQuery.of(context).size;
+    return Card(
+      elevation: 8,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        height: 320,
+        width: deviceSize.width * 0.75,
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'E-mail'),
+                keyboardType: TextInputType.emailAddress,
+                onSaved: (email) => _authData['email'] = email ?? '',
+                validator: (email) {
+                  final email = email ?? '';
+                  if (email.trim().isEmpty || !email.contains('@')) {
+                    return 'Informe um e-mail válido';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Senha'),
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: true,
+                onSaved: (password) => _authData['password'] = password ?? '',
+                controller: _passwordController,
+                // ignore: no_leading_underscores_for_local_identifiers
+                validator: (_password) {
+                  final password = _password ?? '';
+                  if (password.trim().isEmpty || password.length < 5) {
+                    return 'Informe uma senha válida';
+                  }
+                  return null;
+                },
+              ),
+              if (_authMode == AuthMode.Signup)
+                TextFormField(
+                  decoration:
+                      const InputDecoration(labelText: 'Confirmar Senha'),
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
+                  validator: _authMode == AuthMode.Login
+                      ? null
+                      // ignore: no_leading_underscores_for_local_identifiers
+                      : (_password) {
+                          final password = _password ?? '';
+                          if (password != _passwordController.text) {
+                            return 'Senhas não conferem';
+                          }
+                          return null;
+                        },
+                ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _submit,
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 32,
+                    vertical: 8,
+                  ),
+                ),
+                child: Text(
+                  _authMode == AuthMode.Login ? 'ENTRAR' : 'REGISTRAR',
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
