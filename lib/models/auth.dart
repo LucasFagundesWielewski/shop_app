c8ba0497add918fe3ed179c9efa556e1bcdbdc96
Lucas <lucas.fwielewski@gmail.com>
2024-11-12 23:08:49 -0300

    feat: added login

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -5,7 +5,11 @@ import 'dart:convert';
 class Auth with ChangeNotifier {
   static const url =
       'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBvhsXEcNpLeGifeZKFpnyIE-wBhUtXyq0';
-  Future<void> signup(String email, String password) async {
+
+  Future<void> _authenticate(
+      String email, String password, String urlFragment) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlFragment?key=AIzaSyBvhsXEcNpLeGifeZKFpnyIE-wBhUtXyq0';
     final response = await http.post(
       Uri.parse(url),
       body: json.encode({
@@ -14,5 +18,13 @@ class Auth with ChangeNotifier {
         'returnSecureToken': true,
       }),
     );
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
